3fe8e5789ad29a2ecfe833ad84ad38c8eabe9965
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2021-05-16 15:34:32 +1000

    Switchable Flashlight

--- Model Scene/Reef Exploration/GUI.js
+++ Model Scene/Reef Exploration/GUI.js
@@ -53,15 +53,20 @@ function buildGUI() {
     gui = new dat.GUI();
 
     var params = {
-        TimeOfDay_WIP: directionalLight.rotation.z
+        TimeOfDay: directionalLight.rotation.z,
+        Flashlight : flashlight.visible
     }
 
-    gui.add(params, 'TimeOfDay_WIP', 0, 4, .1).onChange(function(rot) {
+    gui.add(params, 'TimeOfDay', 0, 4, .1).onChange(function(rot) {
         // directionalLight.color = Utils.RGB(255 - rot, 255 - rot, 255 - rot);
         // ambientLight.intensity = (rot / 255) * 2;
         directionalLight.position.x = 50 * Math.cos(rot);
         directionalLight.position.y = 50 * Math.sin(rot);
         PerlinGeometry.computeVertexNormals();
+    });
+
+    gui.add(params, 'Flashlight').onChange(function(bOn){
+        flashlight.visible = bOn;
     });
 
 }
